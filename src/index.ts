cb81925673307888f6833eb9a8067bc7f659c872
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-11-10 23:45:47 -0300

    MongoDB connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import app from './app';
+import { connect, disconnect } from 'mongoose';
 
-app.listen(app.get('port'), () => {
-    console.log('Express na porta:', app.get('port'));
-    console.log('Express no modo:', app.get('env'));
-});
+const uriMongoDB = process.env.MONGO_URL || 'mongodb://localhost:27017/';
+
+async function main() {
+    try {
+        await connect(uriMongoDB);
+        console.log('Conectado ao MongoDb Atlas');
+
+        app.listen(app.get('port'), () => {
+            console.log('Express na porta:', app.get('port'));
+            console.log('Express no modo:', app.get('env'));
+        });
+        
+    } catch (error) {
+        console.log('Falha de acesso ao BD:');
+        console.error(error);
+    } finally {
+        await disconnect();
+        console.log('Desconectado do MongoDb Atlas');
+    }
+}
+
+main();
