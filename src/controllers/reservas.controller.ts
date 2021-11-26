c89082113cec499dfbc403556ea3604cabf74734
arthurvergacas <arthurvdm@gmail.com>
2021-11-26 14:28:30 -0300

    Add proper user id to reservation

--- src/controllers/reservas.controller.ts
+++ src/controllers/reservas.controller.ts
@@ -1,26 +1,30 @@
 import { NextFunction, Request, Response } from "express";
-import { validationResult } from "express-validator";
-import { Reserva, ReservaModel } from "../entidades/reserva";
-import { ReservaRepositorio } from "../persistencia/reservaRepositorio";
-import {
-  realizarReserva,
-  verificarDisponibilidade,
-} from "../persistencia/negocio";
+import { Reserva } from "../entidades/reserva";
+import { realizarReserva, verificarDisponibilidade } from "../persistencia/negocio";
 
-export async function register(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+declare namespace Express {
+  interface User {
+    _id?: string;
+  }
+}
+
+export async function register(req: Request, res: Response, next: NextFunction) {
   try {
-    const { idLocador, idAcomodacao, dataDeInicio, dataDeTermino } = req.body;
+    const { idAcomodacao, dataDeInicio, dataDeTermino } = req.body;
+    let idLocador;
+
+    if (req.user) {
+      idLocador = (req.user as Express.User)._id;
+    } else {
+      res.status(401).send("Usuário não está logado.");
+    }
 
     if (idLocador && idAcomodacao && dataDeInicio && dataDeTermino) {
       let reserva: Reserva = await realizarReserva(
         idLocador,
         idAcomodacao,
         dataDeInicio,
-        dataDeTermino,
+        dataDeTermino
       );
       if (reserva) {
         res.json(reserva);
@@ -44,7 +48,7 @@ export async function verify(req: Request, res: Response, next: NextFunction) {
         let verificaReserva: boolean = await verificarDisponibilidade(
           idAcomodacao,
           dataDeInicio,
-          dataDeTermino,
+          dataDeTermino
         );
         if (verificaReserva) {
           res.status(200).send("Ok");
