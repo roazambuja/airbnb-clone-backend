cba8b2f7788799c6e5a7d9d6eadfbf549d1954af
arthurvergacas <arthurvdm@gmail.com>
2021-11-26 11:31:26 -0300

    isAuthenticated now returns the user info if it is logged in

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,39 +2,43 @@ import passport from "passport";
 import { Request, Response, NextFunction } from "express";
 
 export function login(req: Request, res: Response, next: NextFunction) {
-    passport.authenticate("local-login", (err, user, info) => {
-        if (err) return res.status(400).json({ error: err, info });
-        if (!user) return res.status(401).json({ error: "No user found", info });
+  passport.authenticate("local-login", (err, user, info) => {
+    if (err) return res.status(400).json({ error: err, info });
+    if (!user) return res.status(401).json({ error: "No user found", info });
 
-        // criar sessão
-        req.logIn(user, (err) => {
-            if (err) return res.status(401).json({ error: err });
-            return res.status(200).json({ user });
-        });
-    })(req, res, next);
+    // criar sessão
+    req.logIn(user, (err) => {
+      if (err) return res.status(401).json({ error: err });
+      return res.status(200).json({ user });
+    });
+  })(req, res, next);
 }
 
 export function register(req: Request, res: Response, next: NextFunction) {
-    passport.authenticate("local-register", (err, user, info) => {
-        if (err) return res.status(400).json({ error: err, info });
-        if (!user) return res.status(401).json({ error: "No user found", info });
+  passport.authenticate("local-register", (err, user, info) => {
+    if (err) return res.status(400).json({ error: err, info });
+    if (!user) return res.status(401).json({ error: "No user found", info });
 
-        // criar sessão
-        req.logIn(user, (err) => {
-            if (err) return res.status(401).json({ error: err });
-            return res.status(200).json({ user });
-        });
-    })(req, res, next);
+    // criar sessão
+    req.logIn(user, (err) => {
+      if (err) return res.status(401).json({ error: err });
+      return res.status(200).json({ user });
+    });
+  })(req, res, next);
 }
 
 export function ensureAuthentication(req: Request, res: Response, next: NextFunction) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        return res.send(401);
-    }
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.send(401);
+  }
 }
 
 export function isAuthenticated(req: Request, res: Response) {
-    return res.status(200).json({isAuthenticated: req.isAuthenticated()});
+  if (req.isAuthenticated()) {
+    return res.status(200).json({ isAuthenticated: true, user: req.user });
+  } else {
+    return res.status(200).json({ isAuthenticated: false });
+  }
 }
